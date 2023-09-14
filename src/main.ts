db0e8914cce0f212a0a78bf5f088f9aa45cb7a45
Yigithan Colak <yigithancolak97@gmail.com>
2023-09-14 13:33:46 +0300

    fix: Cors origin

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({
-    origin: 'http://localhost:3000'
+    origin: '*'
   })
   app.use(cookieParser())
   await app.listen(8888)
